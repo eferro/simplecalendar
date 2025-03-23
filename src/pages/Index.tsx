9ec6bddb8ee719b1edac13c6dc43bce89ed83cda
Eduardo Ferro <eduardo.ferro@clarity.ai>
2025-03-23 09:46:45 +0100

    Minor format changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,11 +4,7 @@ import Calendar from '../components/Calendar/Calendar';
 
 const Index: React.FC = () => {
   return (
-    <div className="min-h-screen py-8 px-4 md:px-0">
-      <header className="max-w-7xl mx-auto mb-8">
-        <h1 className="text-4xl font-bold tracking-tight text-center">Calendar View</h1>
-      </header>
-
+    <div className="min-h-screen px-4 md:px-0">
       <main>
         <Calendar />
       </main>
